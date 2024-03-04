301c975b6104e1e9ce14446eadfba502c4e33d4a
Gabriel Løsnesløkken <gabriell@uia.no>
2024-03-04 15:09:57 +0100

    Organize more projects into cathegories

---
+++ FeaturedProjects/ExploringIslands/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public float full = 40;
+
+    public void NewHealth(float newHealth)
+    {
+        if (newHealth > full)
+        {
+            newHealth = full;
+            GameManager.instance.health = full;
+        }
+        transform.GetChild(0).localScale = new Vector3 (newHealth / full, 1, 1);
+
+    }
+
+}
---
+++ FeaturedProjects/ExploringIslands/Assets/Scripts/PoisonBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonBar : MonoBehaviour
+{
+    public GameObject sprite;
+    public float spacing = 1.5f;
+
+    public float max = 10;
+
+    public float SpawnChildren(float quantity)
+    {
+        KillAllChildren();
+
+        if (quantity > max)
+        {
+            quantity = max;
+
+        }
+
+        for (int i = 0; i < quantity; i++)
+        {
+            GameObject go = Instantiate(sprite, Vector3.right * i * spacing + transform.position, Quaternion.identity, transform);
+        }
+
+        return quantity;
+    }
+
+    public void KillAllChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+}
