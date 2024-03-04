170088c343e8a47839b677f638f3648cc7d4f2db
Gabriel Løsnesløkken <gabriell@uia.no>
2024-03-04 14:36:53 +0100

    Move UltimateFrisbee to featured

---
+++ FeaturedProjects/UltimateFrisbee/Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    #region Singelton
+    public static GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    public Ball ball;
+
+    public Transform ballsFolder;
+
+    public List<Player> players;
+
+    public Text[] teamsText;
+    public float[] teamsScore;
+
+    private void Start()
+    {
+        teamsScore = new float[teamsText.Length];
+        Score(0, 0);
+        Score(1, 0);
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Restart();
+        }
+    }
+
+    public void Score(int team, float ammount)
+    {
+        teamsScore[team] += ammount;
+        teamsText[team].text = teamsScore[team].ToString();
+    }
+
+    public void Restart()
+    {
+        ball.Detach();
+        ball.transform.position = Vector3.zero;
+        ball.rb.velocity = Vector3.zero;
+        ball.rb.bodyType = RigidbodyType2D.Dynamic;
+
+        foreach(Player player in players)
+        {
+            player.Restart();
+        }
+
+    }
+
+}
---
+++ FeaturedProjects/UltimateFrisbee/Assets/Scripts/Goal.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    public int team;
+
+    public SpriteRenderer sr;
+
+    public Color colorWhenTouchingBall;
+    public Color normalColor;
+
+    private void Start()
+    {
+        normalColor = sr.color;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.parent.parent.CompareTag("Ball"))
+        {
+            Ball ball = collision.transform.parent.parent.GetComponent<Ball>();
+
+            if (ball.attachedTo != null)
+            {
+                if(ball.attachedTo.team != team)
+                {
+                    GameManager.instance.Restart();
+                    GameManager.instance.Score(team, 1);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.parent.parent.CompareTag("Ball"))
+        {
+            sr.color = colorWhenTouchingBall;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.parent.parent.CompareTag("Ball"))
+        {
+            sr.color = normalColor;
+        }
+    }
+
+}
